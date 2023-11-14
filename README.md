e3a36fd83f69f9693bde308159d2c521bde7943a
solsolet <gsl21@alu.ua.es>
2023-11-14 17:51:34 +0100

    Segona part

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Reptes-V1
+
+## Amstrad
+
+## C++
+### Reto1C++
+
+Clonar modelo - IIII
+Modelo funcionando - III
+
--- Reto1C++/src/main.cpp
+++ Reto1C++/src/main.cpp
@@ -7,18 +7,27 @@ struct Pixel {
    uint16_t x{}, y{};
 };
  
-// Contendrá el array de 100 píxeles
 // Dará acceso al array devolviendolo por referencia constante
 struct EntityManager {
-   // TODO
-   std::array<Pixel, 100> pixeles;
+   EntityManager() {
+      for(auto& p: pixeles_) {
+         p.x = rand() % 800;
+         p.y = rand() % 600;
+      }
+   }
+ 
+   std::array<Pixel, 100> const& getPixeles() const { return pixeles_; }
+private:
+   std::array<Pixel, 100> pixeles_{};
 };
  
 // Recibira como parámetro una referencia al EntityManager
 // Le pedirá el array al entity manager y lo pintará
-void render_system(EntityManager pix) { 
+void render_system(EntityManager& EM) {
    BeginDrawing();
-   for(auto& p: pix.pixeles) { DrawPixel(pix.pixeles.x, pix.pixeles.y, RED); }
+   for(auto& p: EM.getPixeles()) {
+      DrawPixel(p.x, p.y, RED);
+   }
    EndDrawing();
 }
  
@@ -26,16 +35,10 @@ void render_system(EntityManager pix) {
 // Pásarselo al RenderSystem
 int main() {
    InitWindow(800, 600, "My Game");
-
-   EntityManager entityMan;
-
-   for(auto& p: entityMan.pixeles) {
-      p.x = rand() % 800;
-      p.y = rand() % 600;
-   }
+   EntityManager  EM{};
  
    while ( ! WindowShouldClose() ) {
-      render_system(entityMan);
+      render_system(EM);
    }
  
    CloseWindow();
